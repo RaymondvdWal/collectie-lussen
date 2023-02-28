debe3a14a6670d28b6faa012c024db90ef392365
RaymondvdWal <Raymondvdwal88@gmail.com>
2023-02-28 16:35:50 +0100

    Huiswerk gemaakt plus bonus opdracht

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,62 @@
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
+        HashSet<Integer> secretNumber = randomNumberGenerator();
+        String number = randomStringGenerator(secretNumber);
+        feedback(number);
+    }
 
-        // Hier mag je je code scrijven voor de hoofd-opdracht
+    public static HashSet<Integer> randomNumberGenerator() {
+        Random random = new Random();
+        HashSet<Integer> randomNumber = new HashSet<>();
+        while (randomNumber.size() < 4){
+            int x = random.nextInt(10);
+            randomNumber.add(x);
+        }
+        return randomNumber;
+    }
 
-        /* deze regel mag je weg halen voor de bonus opdracht. Onderstaande code is voor de bonus opdracht.
-        HashSet<Integer> secretnumber = randomnumbergenerator();
-        String stringnumber =  setToStringConverter(secretnumber);
-        System.out.println(stringnumber);
-        feedback();
-         deze regel mag je weg halen voor de bonus opdracht */
+    public static String randomStringGenerator(HashSet<Integer>randomNumber){
 
+        StringBuilder newString = new StringBuilder();
+
+        for (Integer x : randomNumber) {
+            newString.append(x.toString());
+        }
+        return newString.toString();
     }
 
     /*
      Deze methode is voor de bonus opdracht.
      */
     public static void feedback(String stringnumber) {
+
         Scanner scanner = new Scanner(System.in);
         StringBuilder feedback = new StringBuilder();
-        System.out.println("take a guess");
-        String guess = scanner.nextLine();
-        if (Objects.equals(guess, stringnumber)) {
-            System.out.println("gefeliciteerd je hebt het goed");
-        } else {
+        boolean guessing = true;
+        while (guessing) {
+            System.out.println("take a guess");
+            String guess = scanner.nextLine();
             for (int i = 0; i < 4; i++) {
                 if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
                     feedback.append("+");
                 } else if (stringnumber.contains(guess.substring(i, i + 1))) {
                     feedback.append("0");
-                } else {
-                    feedback.append("X");
+                } else feedback.append("X");
+                if (Objects.equals(guess, stringnumber)) {
+                    System.out.println("gefeliciteerd je hebt het goed");
+                    feedback.delete(i,i+1);
+                    guessing = false;
+                    break;
                 }
-            }
+            }System.out.println(feedback);
+            feedback.setLength(0);
         }
-        System.out.println(feedback.toString());
     }
 }
